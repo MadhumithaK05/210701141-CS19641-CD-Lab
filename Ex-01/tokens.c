3f3a4047a74a73de9181dcb9c9baa51933ab8ca8
Madhumitha K <90406259+MadhumithaK05@users.noreply.github.com>
2024-05-24 18:48:33 +0530

    Update tokens.c

--- Ex-01/tokens.c
+++ Ex-01/tokens.c
@@ -1,50 +1,71 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<stdlib.h>
-#include<string.h>
-#include<math.h>
-void main()
-{
-int i=0,j=0,x=0,n;
-void *p,*add[5];
-char ch,srch,b[15],d[15],c;
-printf("Expression terminated by $:");
-while((c=getchar())!='$')
-{
-b[i]=c;
-i++;
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int iskey(char t[]) {
+    char key[32][15] = {"int","auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
+    for(int i = 0; i < 32; i++) {
+        if(strcmp(key[i], t) == 0) {
+            return 1;
+        }
+    }
+    return 0;
 }
-n=i-1;
-printf("Given Expression:");
-i=0;
-while(i<=n)
-{
-printf("%c",b[i]);
-i++;
+
+int main() {
+    char s[1000];
+    printf("Enter C code (press Ctrl+D to end input):\n");
+    while (fgets(s, sizeof(s), stdin) != NULL) {
+        char temp[30];
+        int j = 0;
+        printf("\n");
+        for(int i = 0; i < strlen(s); i++) {
+            char x = s[i];
+            switch(x) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+                case '=':
+                case ';':
+                case ',':
+                case '(':
+                case ')':
+                case '{':
+                case '}':
+                case '[':
+                case ']':
+                    if(j != 0) {
+                        temp[j] = '\0';
+                        j = 0;
+                        if(iskey(temp) == 1 && strcmp(temp, "") != 0) {
+                            printf("%s Keyword\n", temp);
+                        }
+                        else if(strcmp(temp, "") != 0) {
+                            printf("%s Identifier\n", temp);
+                        }
+                    }
+                    if(x != ' ') {
+                        printf("%c %s\n", x, (x == '=' ? "Operator" : "Delimiter"));
+                    }
+                    break;
+                default:
+                    if(isalpha(x)) {
+                        temp[j++] = x;
+                    }
+                    else if((x == ' ' || x == ';') && j != 0) {
+                        temp[j] = '\0';
+                        j = 0;
+                        if(iskey(temp) == 1 && strcmp(temp, "") != 0) {
+                            printf("%s Keyword\n", temp);
+                        }
+                        else if(strcmp(temp, "") != 0) {
+                            printf("%s Identifier\n", temp);
+                        }
+                    }
+            }
+        }
+    }
+    return 0;
 }
-printf("\n Symbol Table\n");
-printf("Symbol \t addr \t type");
-while(j<=n)
-{
-c=b[j];
-if(isalpha(toascii(c)))
-{
-p=malloc(c);
-add[x]=p;
-d[x]=c;
-printf("\n%c \t %d \t identifier\n",c,p);
-x++;
-j++;
-}
-else
-{
-ch=c;
-if(ch=='+'||ch=='-'||ch=='*'||ch=='=')
-{
-p=malloc(ch);
-add[x]=p;
-d[x]=ch;
-printf("\n %c \t %d \t operator\n",ch,p);
-x++;
-j++;
-}}}}
